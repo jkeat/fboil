e4ea6c8d4c3c2b7a3307295a29ba94798d9f86e4
Jack Keating <keatjm@gmail.com>
2015-07-11 14:34:54 -0500

    app_settings config var for app creation

--- shell.py
+++ shell.py
@@ -3,7 +3,7 @@ import os
 from flask import *
 from app import *
 
-app = create_app('config.development')
+app = create_app(os.environ['APP_SETTINGS'])
 app.test_request_context().push()
 
 # run with the -i flag to enter shell after executing
