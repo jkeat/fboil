7c0f3012e8c2586d503d8105ed75b3a24f5245bd
Jack Keating <keatjm@gmail.com>
2015-07-20 13:14:57 -0500

    .get() environ vars

--- app/controllers/users.py
+++ app/controllers/users.py
@@ -35,8 +35,8 @@ twitter = oauth.remote_app('twitter',
     request_token_url='https://api.twitter.com/oauth/request_token',
     access_token_url='https://api.twitter.com/oauth/access_token',
     authorize_url='https://api.twitter.com/oauth/authenticate',
-    consumer_key=current_app.config["TWITTER_CONSUMER_KEY"],
-    consumer_secret=current_app.config["TWITTER_CONSUMER_SECRET"]
+    consumer_key=os.environ.get("TWITTER_CONSUMER_KEY"),
+    consumer_secret=os.environ.get("TWITTER_CONSUMER_SECRET")
 )
 
 @twitter.tokengetter
